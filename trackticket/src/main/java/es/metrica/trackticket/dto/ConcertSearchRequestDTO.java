75a3eef0d12267299f8b1d6c1483f3162eb6500a
devTighadouini <iiliaastighadouini@gmail.com>
2026-05-20 13:03:18 +0200

    feature: nuevos DTO's añadidos

---
+++ trackticket/src/main/java/es/metrica/trackticket/dto/ConcertSearchRequestDTO.java
@@ -0,0 +1,22 @@
+package es.metrica.trackticket.dto;
+
+import java.time.LocalDate;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record ConcertSearchRequestDTO(
+		
+		@NotNull(message = "La fecha es oligatoria")
+		LocalDate date,
+		
+		@Positive(message = "Identificador de usuario debe ser positivo")
+		Long artista,
+		
+		@Valid
+		VenueDTO venue
+		
+		) {
+
+}
---
+++ trackticket/src/main/java/es/metrica/trackticket/dto/CountResponseDTO.java
@@ -0,0 +1,12 @@
+package es.metrica.trackticket.dto;
+
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record CountResponseDTO(
+		
+		@PositiveOrZero
+		Integer amount
+		
+		) {
+
+}
