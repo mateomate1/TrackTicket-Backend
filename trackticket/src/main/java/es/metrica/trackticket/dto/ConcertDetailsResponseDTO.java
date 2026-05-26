e3818e173c7f2ef519ba30005a9c3655f94abeab
Antonio <antonio.munozpr@metrica-global.com>
2026-05-26 12:25:26 +0200

    Creo el dto de details

--- trackticket/src/main/java/es/metrica/trackticket/dto/ConcertDetailsResponseDTO.java
+++ trackticket/src/main/java/es/metrica/trackticket/dto/ConcertDetailsResponseDTO.java
@@ -2,14 +2,16 @@ package es.metrica.trackticket.dto;
 
 import java.time.LocalDate;
 
+
 public record ConcertDetailsResponseDTO(
 		
-		String nombre,
-		LocalDate date,
-		VenueDTO venue,
-		ArtistResponseDTO artista,
-		String linkArtista,
-		String linkCompra
+		String idTicketMaster,
+		String concertName,
+		LocalDate concertDate,
+		String buyLink,
+		String artistName,
+		String artistLink,
+		VenueDTO venue
 		
 		) 
 		{}
