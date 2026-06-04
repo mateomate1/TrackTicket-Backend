bd52fe6c50f9364ca129e0410f876d246bb4998e
Antonio <antonio.munozpr@metrica-global.com>
2026-06-04 08:40:04 +0200

    Cambios

--- trackticket/src/main/java/es/metrica/trackticket/services/ConcertNotificationSchedulerService.java
+++ trackticket/src/main/java/es/metrica/trackticket/services/ConcertNotificationSchedulerService.java
@@ -1,6 +1,17 @@
 package es.metrica.trackticket.services;
 
+import java.util.List;
+
+import es.metrica.trackticket.dto.CountResponseDTO;
+import es.metrica.trackticket.dto.NotificationRequestDTO;
+import es.metrica.trackticket.dto.NotificationResponseDTO;
+import es.metrica.trackticket.dto.TokenRequestDTO;
+
 public interface ConcertNotificationSchedulerService {
 
 	void checkUpcomingConcerts();
+	List<NotificationResponseDTO> getNotifications(TokenRequestDTO dto);
+	CountResponseDTO countUnread(TokenRequestDTO dto);
+	void readNotification(NotificationRequestDTO dto);
+	void deleteNotification(NotificationRequestDTO dto);
 }
